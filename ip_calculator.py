72d443ba94e054143627126094def677291f96db
colspan <collin@collindigm.com>
2023-05-29 09:37:02 +0300

    Getting the IP Calculator to to get the IP Address and subnet mask as input and return the Network ID, Broadcast address and a list of all the host addresses in the subnet.

--- ip_calculator.py
+++ ip_calculator.py
@@ -1,3 +1,29 @@
-def caculate_network_address(ip_address, network_mask):
+import ipaddress
 
-    return ip_address
+def ip_caculator(ip_address, subnet_mask):
+    network = ipaddress.ip_network(ip_address + '/' + subnet_mask)
+
+    network_id = network.network_address
+    broadcast_address = network.broadcast_address
+
+    host_addresses = []
+
+    for host in network.hosts():
+        host_addresses.append(host)
+
+    return network_id, broadcast_address, host_addresses
+
+
+if __name__ == '__main__':
+    ip_address = input('Enter the IP Address: ')
+    subnet_mask = input('Enter the subnet_mask: ')
+
+    network_id, broadcast_address, host_addresses = ip_caculator(ip_address, subnet_mask)
+
+    print('Network ID:', network_id)
+    print('Broadcast address:', broadcast_address)
+
+    print('Host address:')
+    for host in host_addresses:
+        print(host)
+
